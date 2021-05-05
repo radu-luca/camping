573e05909e4248204f0dc8f1640acdeb34751211
radu-luca <79149783+radu-luca@users.noreply.github.com>
2021-05-05 13:50:43 +0300

    update server

--- controllers/campController.js
+++ controllers/campController.js
@@ -1,10 +1,9 @@
-const path  = require('path');
 const fs = require('fs');
-exports.getHome = () => {
-    var test = "asda";
-fs.readFile(path.join(__dirname,"..","views/index.html"),
-(err,data) => {
-    return data.toString();
-});
-return test;
-};
+const path = require('path');
+
+exports.getCamps = (req, res) =>{
+    fs.readFile(path.join(__dirname, "..", "views/index.html"), (err, content) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content, "utf8");
+    })
+}
--- sever.js
+++ sever.js
@@ -1,15 +1,39 @@
 const http = require("http");
+const path = require("path");
 const fs = require("fs");
+const CampsController = require("./controllers/campController");
 
-const campController = require("./controllers/campController");
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    CampsController.getCamps(req, res);
+  } else if (req.url.indexOf(".") != -1) {
+    var filePath = "." + req.url;
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
+    };
+    var contentType = mimeTypes[extname];
 
-http
-  .createServer((req, res) => {
-    if (req.url === "/" && req.method === "GET") {
-        let htmlCon = campController.getHome();
-        console.log(htmlCon);
-      res.writeHead(200,{ "Content-Type": "text/html" });
-      res.end(htmlCon);
-    }
-  })
-  .listen(3000);
+    fs.readFile(filePath, function (error, content) {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    });
+  }
+});
+
+const PORT = 5000;
+
+server.listen(PORT);
